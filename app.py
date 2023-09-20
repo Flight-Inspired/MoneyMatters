404c7aab06df8cf57ea950747d03fdd5019156ad
Brody <80214001+lurkny@users.noreply.github.com>
2023-09-20 19:28:59 +0000

    cleaned imports

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-#app.py
-from congress_api import get_all_members, insert_members_into_database
-from database_setup import setup_database
+# app.py
 from flask import Flask, render_template, request
-from opensecrets_api import insert_legislators_for_all_states
 from search import search_legislators_by_state, get_top_donors, search_by_legislator, name_to_bioguide_id, search_by_company, parse_company_contributions
-import os, sqlite3
+
 
 app = Flask(__name__)
 
@@ -17,10 +14,12 @@ app = Flask(__name__)
 '''
 
 # Landing page
+
+
 @app.route('/', methods=['GET'])
 def home():
     search_results = None
-    
+
     state_code = request.args.get('state_code')
     if state_code and len(state_code) == 2:
         search_results = search_legislators_by_state(state_code)
@@ -29,7 +28,7 @@ def home():
         return render_template('index.html', search_results=search_results)
     state_code = request.args.get('name')
     if state_code:
-        search_results=search_by_legislator(state_code)
+        search_results = search_by_legislator(state_code)
         topdon = get_top_donors(name_to_bioguide_id(state_code))
         return render_template('single_legislator.html', legislator_details=search_results,  top_donors=topdon)
     state_code = request.args.get('company')
@@ -39,9 +38,7 @@ def home():
         top_contib = parse_company_contributions(search_results)
         return render_template('company_activity.html', legislator_details=search_results, top_donors=top_contib)
     return render_template('index.html', search_results=None)
-    
 
-    
 
 # Legislator details
 @app.route('/legislator/<bioguide_id>', methods=['GET'])
@@ -53,11 +50,10 @@ def legislator_details(bioguide_id):
         if result[14] == bioguide_id:
             legislator_details = result
             break
-    
+
     # Fetch top 10 donors for the legislator from the database
     top_donors = get_top_donors(bioguide_id)
     return render_template('legislator_details.html', legislator_details=legislator_details, top_donors=top_donors)
-
 
 
 if __name__ == '__main__':
