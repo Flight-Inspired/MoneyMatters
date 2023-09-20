b8c01a4873e368ecdfb6d7ead9cddf62da51c63b
ticusb <leviticusb02@gmail.com>
2023-09-19 20:00:24 -0600

    Add /map page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './components/home/Home';
+import Map from './components/map/Map';
 import LegislatorDetails from './components/legislatorDetails/LegislatorDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,8 +11,8 @@ function App() {
     <Router>
         <Routes>
             <Route path="/" exact element={ <Home serverUrl={server_url} /> } />
-    
-            <Route path="/legislator/:bioguideId" element={ <LegislatorDetails serverUrl={server_url} /> } /> {/* Route with a parameter */}
+            <Route path="/legislator/:bioguideId" element={ <LegislatorDetails serverUrl={server_url} /> } />
+            <Route path="/map" element={<Map />} />
         </Routes>
     </Router>
   );
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -54,6 +54,7 @@ function Home(props) {
             )}
                 
             </div>
+            
         </div>
     );
 }
