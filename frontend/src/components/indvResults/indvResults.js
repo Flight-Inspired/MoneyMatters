3bc1b72cfa86e9bf9cf0e863d5294884590f71c8
Brody Larson <larsonbm@colostate.edu>
2023-11-19 10:15:29 -0700

    number formatting

--- frontend/src/components/indvResults/indvResults.js
+++ frontend/src/components/indvResults/indvResults.js
@@ -6,13 +6,14 @@ export default function IndvResults(props) {
   const city = member[3];
   const state = member[4];
 
+
   return (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">
           <strong>{name}</strong>
         </h5>
-        <p className="card-text">{"Amount: " + amt}</p>
+        <p className="card-text">{"Amount: " + amt.toLocaleString("en-US")}</p>
         <p className="card-text">{"Date: " + date}</p>
         <p className="card-text">{"Location: "  + city + ", " + state}</p>
       </div>
