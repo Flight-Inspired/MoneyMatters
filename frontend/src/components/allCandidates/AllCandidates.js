44fd51bfc1f2cad05a77976a36f909effbf3e065
JacopoFB <jacopofb03@gmail.com>
2023-11-27 11:32:38 -0700

    collapse-viz-organization

--- frontend/src/components/allCandidates/AllCandidates.js
+++ frontend/src/components/allCandidates/AllCandidates.js
@@ -1,73 +1,53 @@
-import React from 'react';
-import Tableau from "tableau-react";
+import React, { useEffect, useState } from 'react';
+import TableauViz from '../tableau/TableauViz';
+import ChevronRight from '../icons/ChevronRight';
+import ChevronDown from '../icons/ChevronDown';
+import './allCandidates.css';
+
+
+function VizCollapse(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const content = isOpen ? <TableauViz url={ props.viz.url } /> : <div />;
+    const icon = isOpen ? <ChevronDown /> : <ChevronRight />;
+
+    return (
+        <div className="viz_container">
+            <div className="viz_container_header mt-5" >
+                <div>
+                    { icon }
+                </div>
+                <h3 onClick={() => setIsOpen(!isOpen)} className='h3'>{ props.viz.name }</h3>
+            </div>
+            <div>
+                <p>{ content }</p>
+            </div>
+        </div>
+    );
+}
 
 function AllCandidates(props) {
-    function AllCandidatesHeatMap(props) {
-        const url = "https://public.tableau.com/views/Cand-to-Itcon-HeatmapBoxplot/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
-        
-        const options = {
-            hideTabs: true,
-            hideToolbar: true
-        };
-
-        return (
-            <Tableau
-                url={url}
-                options={options}
-        />
-        );
-
-    }
-
-    function RepublicanCommiteesHistogram(props) {
-        const url = "https://public.tableau.com/views/RepublicanCommittees/RepCommitteesDB?:language=en-US&:display_count=n&:origin=viz_share_link";
-        
-        const options = {
-            hideTabs: true,
-            hideToolbar: true
-        };
-
-        return (
-            <Tableau
-                url={url}
-                options={options}
-        />
-        );
-
-    }
-
-    function DemocraticCommitteeHistogram(props) {
-        const url = "https://public.tableau.com/views/DemocratCommittees/DemCommitteesbyOccupationDB?:language=en-US&:display_count=n&:origin=viz_share_link";
-        
-        const options = {
-            hideTabs: true,
-            hideToolbar: true
-        };
-
-        return (
-            <Tableau
-                url={url}
-                options={options}
-        />
-        );
-
-    }
+    const visuals = [
+        {
+            name: "HeatMap of Candidates",
+            url: "https://public.tableau.com/views/Cand-to-Itcon-HeatmapBoxplot/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link"
+        },
+        {
+            name: "Top Democratic Donations",
+            url: "https://public.tableau.com/views/DemocratCommittees/DemCommitteesbyOccupationDB?:language=en-US&:display_count=n&:origin=viz_share_link"
+        },
+        {
+            name: "Top Republican Donations",
+            url: "https://public.tableau.com/views/RepublicanCommittees/RepCommitteesDB?:language=en-US&:display_count=n&:origin=viz_share_link"
+        }
+    ];
 
     return (
         <div className='container-fluid m-auto' style={{ 'maxWidth': 1200 + 'px' }}>
-            <div className='container-md mx-auto mt-3'>
-                <h3 className='h3'>HeatMap of Candidates:</h3>
-                <AllCandidatesHeatMap/>
-            </div>
             <div className='container-md mx-auto mt-5'>
-                <hr className="border border-primary border-3 opacity-75" />
-                <h3 className='h3'>Top Democratic Donations:</h3>
-                <DemocraticCommitteeHistogram/>
-            </div>
-            <div className='container-md mx-auto mt-5'>
-                <hr className="border border-danger border-2 opacity-50" />
-                <h3 className='h3'>Top Republican Donations:</h3>
-                <RepublicanCommiteesHistogram/>
+                {visuals.map((viz) => (
+                    <VizCollapse viz={viz} />
+                ))}
             </div>
         </div>
     );
