1a0f343fd999bad040f14a7495c435dd90a85b4c
JacopoFB <jacopofb03@gmail.com>
2023-11-28 17:08:00 -0700

    thumbnails

--- frontend/src/components/allCandidates/AllCandidates.js
+++ frontend/src/components/allCandidates/AllCandidates.js
@@ -4,6 +4,9 @@ import ChevronRight from '../icons/ChevronRight';
 import ChevronDown from '../icons/ChevronDown';
 import './allCandidates.css';
 
+// thumbnails
+import allCandidateHeatmapThumb from '../../assets/heatmap_candidate_thumbnail.PNG';
+
 
 function VizCollapse(props) {
     const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +21,8 @@ function VizCollapse(props) {
                     { icon }
                 </div>
                 <h3 onClick={() => setIsOpen(!isOpen)} className='h3'>{ props.viz.name }</h3>
+                <div className="flex_placeholder"></div>
+                <img src={ props.viz.thumbnail } />
             </div>
             <div>
                 <p>{ content }</p>
@@ -30,14 +35,17 @@ function AllCandidates(props) {
     const visuals = [
         {
             name: "HeatMap of Candidates",
+            thumbnail: allCandidateHeatmapThumb,
             url: "https://public.tableau.com/views/Cand-to-Itcon-HeatmapBoxplot/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link"
         },
         {
             name: "Top Democratic Donations",
+            thumbnail: allCandidateHeatmapThumb,
             url: "https://public.tableau.com/views/DemocratCommittees/DemCommitteesbyOccupationDB?:language=en-US&:display_count=n&:origin=viz_share_link"
         },
         {
             name: "Top Republican Donations",
+            thumbnail: allCandidateHeatmapThumb,
             url: "https://public.tableau.com/views/RepublicanCommittees/RepCommitteesDB?:language=en-US&:display_count=n&:origin=viz_share_link"
         }
     ];
